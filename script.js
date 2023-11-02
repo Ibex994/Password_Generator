701783ea34ceff723753ebf743f8613954bd08b9
Biruk Nigussie <biruknigussie994@gmail.com>
2023-11-02 09:26:16 +0300

    added a copy button and a function, made the pasword to satrt with capital letters only

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.getElementById("passgeneretor").onclick=function()
 { 
-    let characters="ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-123456789abcdefghijklmnopqrstuvwxyz";
+    let characters="!@#$%^&*()_+-123456789abcdefghijklmnopqrstuvwxyz";
+    let capital="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let length=document.getElementById("select").value;
     let randomString ="";
+    let firstCharIndex=Math.floor(Math.random()*capital.length)
+    randomString+=capital.charAt(firstCharIndex)
     for(let i=1;i<=length;i++)
 {
    let rnum= Math.floor(Math.random() * characters.length);
@@ -12,4 +15,11 @@ document.getElementById("passgeneretor").onclick=function()
 }
 document.getElementById("clearbutton").onclick=function(){
     document.getElementById("tetx").value='';
+}
+
+document.getElementById("copybtn").onclick=function(){
+    let copyText=document.getElementById("tetx");
+    copyText.select();
+     document.execCommand("copy");
+    alert("Password Copied")
 }
